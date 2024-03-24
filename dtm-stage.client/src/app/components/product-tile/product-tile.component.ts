a1ba984b953287f85d98a3ad4ed67c4356ce6281
awakentoast <luuk.bouma01@gmail.com>
2024-03-24 23:23:17 +0100

    indented the relevant code for code review bc i didnt commit properly

--- dtm-stage.client/src/app/components/product-tile/product-tile.component.ts
+++ dtm-stage.client/src/app/components/product-tile/product-tile.component.ts
@@ -1,44 +1,38 @@
-import { Component, OnInit, signal } from '@angular/core';
-import { ProductService } from '../../services/product.service';
-import { Product } from '../../models/product';
-import { Item } from '../../models/item';
-import { Image } from '../../models/image';
-import { CommonModule } from '@angular/common';
-import { Observable } from 'rxjs';
+  import { Component, OnInit, signal } from '@angular/core';
+  import { ProductService } from '../../services/product.service';
+  import { Product } from '../../models/product';
+  import { Item } from '../../models/item';
+  import { Image } from '../../models/image';
+  import { CommonModule } from '@angular/common';
+  import { Observable } from 'rxjs';
 
-@Component({
-  imports: [CommonModule],
-  standalone: true,
-  selector: 'product-tiles',
-  templateUrl: './product-tile.component.html',
-  styleUrl: './product-tile.component.css'
-})
+  @Component({
+    imports: [CommonModule],
+    standalone: true,
+    selector: 'product-tiles',
+    templateUrl: './product-tile.component.html',
+    styleUrl: './product-tile.component.css'
+  })
 
-export class ProductTileComponent implements OnInit {
-  public products$: Observable<Product[]> = new Observable<Product[]>;
+  export class ProductTileComponent implements OnInit {
+    public products$: Observable<Product[]> = new Observable<Product[]>;
 
-  constructor(private productService: ProductService) { }
+    constructor(private productService: ProductService) { }
 
-  ngOnInit(): void {
-    // this.productService.subscribeToProducts({
-    //   next: product => this.products.push(product),
-    //   error: err => console.log(err),
-    //   complete: () => console.log("observer completed"),
-    // });
+    ngOnInit(): void {
+      this.products$ = this.productService.getProducts()
+    }
 
-    this.products$ = this.productService.getProducts()
+    AddProduct(): void {
+      console.log("a");
+      this.productService.addProduct(new Product(
+          5,
+          "5",
+          "lorum",
+          "Ipsum",
+          "ada",
+          [new Item(1, 3)],
+          [new Image(5, "5" + ".png", "afbeelding " + (5).toString())]
+        ));
+    }
   }
-
-  AddProduct(): void {
-    console.log("a");
-    this.productService.addProduct(new Product(
-        5,
-        "5",
-        "lorum",
-        "Ipsum",
-        "ada",
-        [new Item(1, 3)],
-        [new Image(5, "5" + ".png", "afbeelding " + (5).toString())]
-      ));
-  }
-}
--- dtm-stage.client/src/app/services/product.service.ts
+++ dtm-stage.client/src/app/services/product.service.ts
@@ -1,52 +1,52 @@
-import { Injectable, signal } from '@angular/core';
-import { Product } from '../models/product';
-import { Item } from '../models/item';
-import { Image } from '../models/image';
-import { Observable, map, mergeAll, BehaviorSubject } from 'rxjs';
+  import { Injectable, signal } from '@angular/core';
+  import { Product } from '../models/product';
+  import { Item } from '../models/item';
+  import { Image } from '../models/image';
+  import { Observable, map, mergeAll, BehaviorSubject } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class ProductService {
-  private products$: BehaviorSubject<Product[]> = new BehaviorSubject<Product[]>([]);
+  @Injectable({
+    providedIn: 'root'
+  })
+  export class ProductService {
+    private products$: BehaviorSubject<Product[]> = new BehaviorSubject<Product[]>([]);
 
-  constructor() {
-    this.productsMock();
+    constructor() {
+      this.productsMock();
+    }
+
+    public getProduct(productId: number): Observable<Product> {
+      return this.products$.pipe(
+        map(products => products.filter(product => product.id === productId)),
+        mergeAll()
+      );
+    }
+
+    public getProductIds(): Observable<number[]> {
+      return this.products$.pipe(
+        map(products => products.map(product => product.id))
+      );
+    }
+
+    public getProducts(): Observable<Product[]> {
+      return this.products$.asObservable();
+    }
+
+    public addProduct(product: Product): void {
+      const products = this.products$.getValue();
+      products.push(product);
+      this.products$.next(products);
+    }
+
+    private productsMock(): void {  
+      this.products$.next([0, 1, 2, 3, 4].map(
+        i => new Product(
+          i,
+          i.toString(),
+          "lorum",
+          "Ipsum",
+          "ada",
+          [new Item(1, 3)],
+          [new Image(i, i.toString() + ".png", "afbeelding " + (i).toString())]
+        )));    
+    }
   }
-
-  public getProduct(productId: number): Observable<Product> {
-    return this.products$.pipe(
-      map(products => products.filter(product => product.id === productId)),
-      mergeAll()
-    );
-  }
-
-  public getProductIds(): Observable<number[]> {
-    return this.products$.pipe(
-      map(products => products.map(product => product.id))
-    );
-  }
-
-  public getProducts(): Observable<Product[]> {
-    return this.products$.asObservable();
-  }
-
-  public addProduct(product: Product): void {
-    const products = this.products$.getValue();
-    products.push(product);
-    this.products$.next(products);
-  }
-
-  private productsMock(): void {  
-    this.products$.next([0, 1, 2, 3, 4].map(
-      i => new Product(
-        i,
-        i.toString(),
-        "lorum",
-        "Ipsum",
-        "ada",
-        [new Item(1, 3)],
-        [new Image(i, i.toString() + ".png", "afbeelding " + (i).toString())]
-      )));    
-  }
-}
