c53e744c6f648504e03527674ff08e726a3a5123
awakentoast <luuk.bouma01@gmail.com>
2024-03-25 13:56:07 +0100

    added the calendar, has no functionality yet

--- dtm-stage.client/src/app/components/cart-page-components/reservation-calendar/reservation-calendar.component.ts
+++ dtm-stage.client/src/app/components/cart-page-components/reservation-calendar/reservation-calendar.component.ts
@@ -1,9 +1,13 @@
 import { Component, Injectable } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DateInputsModule } from '@progress/kendo-angular-dateinputs';
 
 @Component({
   selector: 'app-reservation-calendar',
   standalone: true,
-  imports: [],
+  imports: [
+    DateInputsModule
+  ],
   templateUrl: './reservation-calendar.component.html',
   styleUrl: './reservation-calendar.component.css'
 })
